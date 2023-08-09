29875c05e8f09ce948f797ffbdce868d7fbf34e4
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-08-09 17:05:51 +0200

    Update

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ def select_exercise():
     print("Select an exercise to solve from the following list: ")
     slide01.CapitalBudget().solve_exercise()
 
+
 if __name__ == '__main__':
     select_exercise()
